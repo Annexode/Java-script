aa6eeddcd7148035f27d2e4ff371e81bce57a526
Annexode <annexode@gmail.com>
2024-08-22 21:29:09 +0200

    JavaScript course journey

---
+++ css/js/app.js
@@ -0,0 +1,8 @@
+// document.write({name: 'Annexode'})
+// alert({name: 'Annexode'})
+// console.log({name:'Annexode'})
+
+console.log('Annexode the editor')
+console.log('Annexode the creator')
+console.log('Annexode the parent')
+console.log('Annexode the lover')
