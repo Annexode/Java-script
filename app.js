170e4d3150793182940f508c29a9208bfdf9bc79
Annexode <annexode@gmail.com>
2024-08-24 02:58:28 +0200

    Javascript journey

---
+++ app.js
@@ -0,0 +1,24 @@
+
+document. write("Annexode the creator")
+console. log('Annexode the creator')
+document.write("Annexode the");
+
+let name = " Annnexode IGIRANEZA  "
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+
+let state, province, district;
+state= "Kigali";
+province= "Rwanda";
+district= "Nyarugenge"
+let umugabo= "Jean Piere de Mutto";
+let Annexode= "Rubyogo";
+console.log(state, province,district);
+console.log(umugabo);
+console.log(Annexode);
--- css/js/app.js
+++
@@ -1,8 +0,0 @@
-// document.write({name: 'Annexode'})
-// alert({name: 'Annexode'})
-// console.log({name:'Annexode'})
-
-console.log('Annexode the editor')
-console.log('Annexode the creator')
-console.log('Annexode the parent')
-console.log('Annexode the lover')
